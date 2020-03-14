e3011df45fa832b500dfbe8ff8f63d579cb77b26
宋均均 <sjj107294@alibaba-inc.com>
2020-03-14 20:53:07 +0800

    feat: init

---
+++ pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="content">
+		<swiper>
+			<swiper-item>
+				<image src="../../static/logo.png"></image>
+			</swiper-item>
+			<swiper-item>
+				<view>2</view>
+			</swiper-item>
+			<swiper-item>
+				<view>3</view>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
